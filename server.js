f804d08f1f7194de82e3178648c1b11e8edbddcd
Dasa Raghavan <dashfamily@gmail.com>
2017-12-12 16:13:37 -0600

    node web server initial commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 var options = {
   dotfiles: 'ignore',
   etag: false,
-  extensions: ['htm', 'html'],
+  extensions: ['htm', 'html', 'hbs'],
   index: false,
   maxAge: '1d',
   redirect: false,
@@ -12,23 +14,56 @@ var options = {
     res.set('x-timestamp', Date.now())
   }
 }
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase(); //new Date().getFullYear();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page',
+//     welcomeMessage: 'This page is under construction ...'
+//   });
+// });
+
 app.use(express.static((__dirname + '/public'), options));
+
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log+'\n', (err) => {
+    if (err) {
+      console.log('Unable to write server.log' + err)
+    }
+  });
+  console.log(log);
+  next();
+});
 
 app.get('/', (req, res) => {
   //res.send('<h1>Hello! World! This is express!</h1>');
-  res.send({
-    name: 'Dasa',
-    likes: [{
-      activities: 'running',
-      hobbies: 'movies'
-    },
-    'Marathons']
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to Some Website'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h2>About Page</h2>');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    aboutMessage: 'This is the about Page'
+
+  });
 });
+
+
 
 app.get('/bad', (req, res) => {
   res.send({
@@ -37,5 +72,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen('3030', () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 3030');
 });
