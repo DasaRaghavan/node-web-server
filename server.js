f023fbf0bf1ba9ae6d3727c0584eb8010a0e8399
Dasa Raghavan <dashfamily@gmail.com>
2017-12-12 16:53:06 -0600

    set up start script and heorku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3030
 
 var app = express();
 var options = {
@@ -71,6 +72,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen('3030', () => {
-  console.log('Server started on port 3030');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
